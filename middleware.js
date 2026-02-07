1b0ca4a9c4e58dfb8a293d704201df19a3c6eb7a
vishwajit15-git <hodgarvishwajit15@gmail.com>
2026-02-07 09:21:30 +0530

    Login after Signup  and  post-Login page

--- middleware.js
+++ middleware.js
@@ -1,7 +1,19 @@
 module.exports.isLoggedIn=(req,res,next)=>{
      if(!req.isAuthenticated()){
-    req.flash("error","Login to create new Listing!");
-    return res.redirect("/login");
+        if (req.session) {
+            req.session.redirectUrl = req.originalUrl;
+        } else {
+            res.locals.redirectUrl = req.originalUrl;
+        }
+        req.flash("error","Login to create new Listing!");
+        return res.redirect("/login");
     }
     next();
 }
+
+module.exports.saveRedirectUrl=(req,res,next)=>{
+    if (req.session && req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
